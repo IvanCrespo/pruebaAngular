dea99556dfc5444a9b28f96974d68332eb50fb91
IvanCrespo <contacto.ivancrespo@gmail.com>
2021-11-26 00:21:14 -0600

    Parte 43

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -2,8 +2,17 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-heroe',
-    templateUrl: 'heroe.component.html' 
+    templateUrl: 'heroe.component.html'
 })
 export class HeroeComponent {
     nombre: string = 'IronMan';
+    edad: number = 32;
+
+    obtenerNombre(): string{
+      return `${ this.nombre} - ${ this.edad}`;
+    }
+
+    get nombreCapitalizado(){
+      return this.nombre.toUpperCase();
+    }
 }
